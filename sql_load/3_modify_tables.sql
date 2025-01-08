2f78c25e271e2565707ede3522121247b6b3bd6b
anil-alc <anilalkan96@gmail.com>
2025-01-08 17:46:10 +0300

    first commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,15 @@
+COPY company_dim
+FROM 'D:\SQL_Project\csv_files\company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM 'D:\SQL_Project\csv_files\skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM 'D:\SQL_Project\csv_files\job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM 'D:\SQL_Project\csv_files\skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
